4d1f80c00c0e77c781decc52fef6fa28cf4ab94a
Sebas21gt <sebasteriogt@gmail.com>
2024-05-15 10:05:12 -0400

    RAID-9-Integration-chatBot

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,42 @@
 <template>
-     <Head>
-        <Title>
-           Raidin App
-        </Title>
-    </Head>
 
-    <main class="flex flex-col lg:flex-row gap-4 h-full">
-      <!-- Bot conversation -->
-      <section class=" flex-auto rounded-lg bg-slate-600 grid place-content-center text-white">
-          bot goes here
-      </section>
+  <Head>
+    <Title>
+      Raidin App
+    </Title>
+  </Head>
 
-      <!-- Car suggestions and filters -->
-      <section class=" flex flex-col flex-auto rounded-lg gap-4 overflow-hidden text-white">
-        <section class="bg-slate-600 flex-auto grid place-content-center rounded-lg">
-          Other stuff
-        </section>
-         <section class="bg-slate-600 flex-auto grid place-content-center rounded-lg">
-          Other stuff
-        </section>
+  <main class="flex flex-col lg:flex-row gap-4 h-full">
+    <!-- Bot conversation -->
+    <section class="flex-auto rounded-lg bg-slate-600 flex relative text-black">
+      <div class="absolute top-0 left-0 w-full p-6 bg-white text-center z-10" style="border-radius: 10px 10px 0 0;">
+        <div class="flex items-center justify-center">
+          <div class="rounded-full h-12 w-12 flex items-center justify-center bg-gray-300 mr-2">
+            <img src="https://res.cloudinary.com/dhicmjsel/image/upload/v1714524931/fapl7n7xtfouekgtidat.jpg"
+              alt="Avatar" class="rounded-full h-full w-full object-cover" />
+          </div>
+          <p>Raidin Bot</p>
+        </div>
+      </div>
+      <iframe src="https://botsito.tiiny.site/" frameborder="0" class="w-full h-full rounded-lg"></iframe>
+    </section>
+
+    <!-- Car suggestions and filters -->
+    <section class=" flex flex-col flex-auto rounded-lg gap-4 overflow-hidden text-white">
+      <section class="bg-slate-600 flex-auto grid place-content-center rounded-lg">
+        Other stuff
       </section>
-    </main>
+      <section class="bg-slate-600 flex-auto grid place-content-center rounded-lg">
+        Other stuff
+      </section>
+    </section>
+  </main>
 
 </template>
 
-<script setup >
+<script setup>
 
 
 </script>
 
-<style>
-</style>
-
+<style></style>
